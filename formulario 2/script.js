e4694fd774a495b5e94ac39d85c8797aa71c3818
Alexander Rojas Urrutia <you@example.com>
2025-06-05 19:42:21 -0500

    primer commit

--- formulario 2/script.js
+++ formulario 2/script.js
@@ -3,7 +3,7 @@ const apellido = document.getElementById("apellido");
 const ciudad = document.getElementById("ciudad");
 const email = document.getElementById("correo");
 
-function enviarFormulario() {
-    alert("nombre: "+ nombre.value + "\napellido: " + apellido.value + "\nciudad: " + ciudad.value + "\nestado: " + estado.value);
-}
+
+
+
 
